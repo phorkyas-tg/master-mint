7e6f5c86759f137326bb93be0def78fc2a44f36a
Tom Gothan <tom.gothan@googlemail.com>
2023-11-19 11:37:07 +0100

    Added Button mechanic

--- js/scenes/level.js
+++ js/scenes/level.js
@@ -1,6 +1,11 @@
 const TILE_WIDTH = 64;
 const TILE_HEIGHT = 64;
 
+const WHITE = "0xffffff"
+const GREY = "0x63666a"
+const ORANGE = "0xed8b00"
+const PETROL = "0x208ca3"
+
 
 class Level extends Phaser.Scene
 {
@@ -13,48 +18,109 @@ class Level extends Phaser.Scene
 
     init (data)
     {
-        this.timer = null;
-        this.updateTime = 120
-        this.animationTime = 500
-        this.candrag = false;
-        this.numberOfBeans = 6
-        this.moves = 1
         this.gameOver = false
-
-        this.collect = {"numberOfCollectibles": 3, 
-                        "0": {"spriteIndex": 0, "collected": 0, "collect": 200, "bean": null, "txt": null},
-                        "1": {"spriteIndex": 1, "collected": 0, "collect": 200, "bean": null, "txt": null},
-                        "2": {"spriteIndex": 2, "collected": 0, "collect": 200, "bean": null, "txt": null}}
+        this.numberOfBeans = 6
     }
 
     preload ()
     {
-        this.load.image('match3_tiles', 'assets/tiles/match3.png')
+        // this.load.image('match3_tiles', 'assets/tiles/match3.png')
         this.load.image('bg', 'assets/sprites/headquarter.png')
-        this.load.image('beanContour', 'assets/sprites/coffee_bean_contour.png')
-        this.load.image('beanOrange', 'assets/sprites/coffee_bean_orange.png')
-        this.load.image('hand', 'assets/sprites/hand_touch.png')
+        // this.load.image('beanContour', 'assets/sprites/coffee_bean_contour.png')
+        // this.load.image('beanOrange', 'assets/sprites/coffee_bean_orange.png')
+        // this.load.image('hand', 'assets/sprites/hand_touch.png')
         this.load.spritesheet('match3Sprite', 'assets/tiles/match3.png', { frameWidth: TILE_WIDTH, frameHeight: TILE_HEIGHT });
 
-        this.load.tilemapTiledJSON('tilemap', 'assets/tiles/level5.json')
+        // this.load.tilemapTiledJSON('tilemap', 'assets/tiles/level5.json')
+
     }
 
     create ()
     {
+        this.beans = new Object()
+        
         const bg = this.add.image(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, "bg").setScale(5).setAlpha(0.2)
-        // const map = this.make.tilemap({ key: 'tilemap' })
-        // const tileset = map.addTilesetImage('match3', 'match3_tiles')
 
-        // const mapLayer = map.createLayer('map', tileset)
-        // const genLayer = map.createLayer('generator', tileset)
-        // const beanLayer = map.createLayer('beans', tileset)
+        // decrease buttons
+        for (let i=0; i < 4; i++) 
+        {
+            let x = 200 + i * 200;
+            let y = CANVAS_HEIGHT/2 + 100;
 
+            let rec = this.add.triangle(x, y, 0, 0, 100, 0, 50, 50, 0xed8b00, 1)
+                .setOrigin(0.5)
+                .setStrokeStyle(5, ORANGE, 0.5)
+                .setInteractive()
 
-        // this.gameBoard = new Object()
-        // this.generators = new Object()
-        // this.beans = new Object()
+            this.updateButton(rec, i, -1)
+        }
+
+        // increase buttons
+        for (let i=0; i < 4; i++) 
+        {
+            let x = 200 + i * 200;
+            let y = CANVAS_HEIGHT/2 - 100;
+
+            let rec = this.add.triangle(x, y, 0, 50, 100, 50, 50, 0, 0xed8b00, 1)
+                .setOrigin(0.5)
+                .setStrokeStyle(5, ORANGE, 0.5)
+                .setInteractive()
+
+            this.updateButton(rec, i, 1)
+        }
+
+        this.initBeans()
+
     }
 
+    initBeans() { 
+        for (let i=0; i < 4; i++) 
+        {
+            let x = 200 + i * 200;
+            let y = CANVAS_HEIGHT/2;
+            
+            let beanIndex = 0
+            this.beans[i] = this.createBean(beanIndex, x, y)
+        }
+    }
+
+    updateButton(rec, buttonIndex, direction) {
+        rec.on('pointerdown', function (pointer)
+        {
+            rec.setFillStyle(PETROL, 1)
+
+            let currentBeanIndex = this.beans[buttonIndex].beanIndex
+            let newBeanIndex = (this.numberOfBeans + currentBeanIndex + direction) % this.numberOfBeans
+            let x = this.beans[buttonIndex].x
+            let y = this.beans[buttonIndex].y
+
+            this.beans[buttonIndex].destroy()
+            this.beans[buttonIndex] = this.createBean(newBeanIndex, x, y)
+            
+        }, this);
+        rec.on('pointerup', function (pointer)
+        {
+            rec.setFillStyle(ORANGE, 1)
+        }, this);
+        rec.on('pointerover', function (pointer)
+        {
+            rec.setStrokeStyle(5, GREY, 0.5)
+        }, this);
+        rec.on('pointerout', function (pointer)
+        {
+            rec.setStrokeStyle(5, ORANGE, 0.5)
+        }, this);
+
+        return rec
+    }
+
+    createBean(beanIndex, x, y) {
+        let bean = this.physics.add.sprite(x, y, 'match3Sprite', beanIndex)
+                .setOrigin(0.5)
+                .setScale(2)
+        bean.beanIndex = beanIndex
+        return bean
+    }
 
 
     // The maximum is inclusive and the minimum is inclusive
